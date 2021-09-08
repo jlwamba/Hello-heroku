f49d67552781b50f44285a527ddc96fa2187a229
yasuke <yasuke@kali.yasuke>
2021-09-07 23:05:20 -0500

    updated readme.md

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 app.get('/', function(req, res) {
 
-    res.send('<h3>Hello Node on Heroku</h3>' & 
-    '<a href="https://github.com/jlwamba/Hello-heroku"')
+    res.send('<h3>Hello Node on Heroku</h3><a href="https://github.com/jlwamba/Hello-heroku" target="_blank">See the code!</a>');
+    res.sendFile('/index.js', { root: __dirname });
+
 
 });
 
